fd544c9e077de6900862c41fb5468286e1916310
Erik Molin <erik.molin@systecongroup.com>
2024-06-04 22:18:53 +0200

    Fix packaging

--- src/test/java/com/erikmolin/DisplayWindowTest.java
+++ src/test/java/com/erikmolin/DisplayWindowTest.java
@@ -1,7 +1,5 @@
 package com.erikmolin;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import com.erikmolin.game.GameOfLife;
 import com.erikmolin.game.board.SquareBoard;
 import org.junit.jupiter.api.Test;
